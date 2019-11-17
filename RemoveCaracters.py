d459c94d73ad00a0496f1399456632b056a12c48
Anaitz98 <anaitz98@gmail.com>
2019-11-17 20:40:37 +0100

    comentar funcionamiento

--- RemoveCaracters.py
+++ RemoveCaracters.py
@@ -1,18 +1,28 @@
 import sys
-with open(sys.argv[1], 'r') as doc:
-    print(doc)
-    removableChars = []
-    for lerroa in doc:
-        print(lerroa)
-        lerroGarbia = lerroa[1:-2].replace("'","")
-        for char in lerroGarbia.split(','):
-            removableChars.append(char)
-newdoc = open("FinalPreArfffrawData2Atrib.txt", 'w+')
-with open(sys.argv[2], 'r') as doc:
-    for lerroa in doc:
-        newline = lerroa.replace("'","")
-        new2line = newline.split(",")
-        for character in removableChars:
-            new2line[2]= new2line[2].replace(character,"")
-        newLine3 =new2line[0]+" "+new2line[1]+","+new2line[2]
-        newdoc.write(newLine3)
+def removeCaracteres(datos, caracteres):
+    with open(datos, 'r') as doc:
+        print(doc)
+        removableChars = []
+        for lerroa in doc:
+            print(lerroa)
+            lerroGarbia = lerroa[1:-2].replace("'","")
+            for char in lerroGarbia.split(','):
+                removableChars.append(char)
+    newdoc = open("FinalPreArfffrawData2Atrib.txt", 'w+')
+    with open(caracteres, 'r') as doc:
+        for lerroa in doc:
+            newline = lerroa.replace("'","")
+            new2line = newline.split(",")
+            for character in removableChars:
+                new2line[2]= new2line[2].replace(character,"")
+            newLine3 =new2line[0]+" "+new2line[1]+","+new2line[2]
+            newdoc.write(newLine3)
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        removeCaracteres(sys.argv[1],sys.argv[2])
+    else:
+        print("La cantidad de atributos es incorrecta.\n" +
+              "Elimina de los datos los caracteres seleccionados y devuelve un fichero llamado FinalPreArfffrawData2Atrib.txts\n" +
+              "Paramentros:\n" +
+              "1.- fichero con los caracteres a eliminar\n"+
+              "2.- path a los datos")
--- characterDetect.py
+++ characterDetect.py
@@ -1,10 +1,19 @@
 import sys
-with open(sys.argv[1], 'r') as doc:
-    newdoc = open("caracters.txt", 'w+')
-    caracters = []
-    for lerroa in doc:
-        new2line = lerroa.split(",")
-        for char in  new2line[2]:
-            if char not in caracters:
-                caracters.append(char)
-    newdoc.write(str(caracters))
+def remove(datos):
+    with open(datos, 'r') as doc:
+        newdoc = open("caracters.txt", 'w+')
+        caracters = []
+        for lerroa in doc:
+            new2line = lerroa.split(",")
+            for char in  new2line[2]:
+                if char not in caracters:
+                    caracters.append(char)
+        newdoc.write(str(caracters))
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        remove(sys.argv[1])
+    else:
+        print("La cantidad de atributos es incorrecta.\n" +
+              "Crea una lista con los caractere contenidos en el texto y los guarda en caracteres.txt\n" +
+              "Paramentros:\n" +
+              "1.- path a los datos")
